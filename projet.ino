72d8637243b18f20c539a928e358e8efe22eeb17
Maxence <maxence.crosse@ynov.com>
2021-04-12 16:08:20 +0200

    MidDay

--- projet.ino
+++ projet.ino
@@ -1,11 +1,49 @@
+#define pinPot A0
+#define LED D1
+
+unsigned long ms;
+unsigned long secondes;
+unsigned long minutes;
+int dayMax;
+int midDay;
+int test;
+int valPot = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  ms = millis();
+  secondes = ms/1000;
+  minutes = secondes/60;
+  Serial.println(secondes);
+  Serial.println(minutes);
 
+
+  valPot = analogRead(pinPot);
+  int valTrans = map(valPot, 0, 1024, 1, 10);
+  dayMax = 600 / valTrans;
+  Serial.println(dayMax);
+  midDay = dayMax/2;
+  test = secondes%midDay;
+  Serial.println("Valeur modulo : ");
+  Serial.println(test);
+  
+
+  if(test == 0){
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+  }
+
+  
+
+  
+  delay(1000);
 }
 
 
@@ -34,4 +72,8 @@ void fumee(){
   
 }
 
-//
+//BARBECUE
+
+void barbecue(){
+  
+}
