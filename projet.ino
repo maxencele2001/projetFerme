d6fde9ad0dbf640f2f80b1d01926610a3dc26f9d
Maxence <maxence.crosse@ynov.com>
2021-04-12 14:40:26 +0200

    First Commit

---
+++ projet.ino
@@ -0,0 +1,37 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+//BASSE COUR
+
+void animPoule(){
+  
+}
+
+void coqChante(){
+  
+}
+
+void vacheMeugle(){
+  
+}
+
+
+//FERME
+
+void ferme(){
+  
+}
+
+void fumee(){
+  
+}
+
+//
